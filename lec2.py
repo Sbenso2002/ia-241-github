db26ce42870aab13d4a1b3387dfa3ac4c705c8b7
EC2 Default User <ec2-user@ip-172-31-42-15.ec2.internal>
2022-01-26 20:04:42 +0000

    lec 2

---
+++ lec2.py
@@ -0,0 +1,24 @@
+"""
+lec 2
+this is a single line comment 
+
+"""
+
+# print("hello world") # this is a single line comment 
+
+#print( type( 123 ) )
+
+#print( type (123.) )
+
+#print( type ("123" ) )
+
+#print( "Tom's cat")
+
+#print ("Hello " +  "World" )
+
+
+
+
+my_str = 'hello world'
+
+print(my_str.upper())
