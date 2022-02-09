a594f7dd0c384db90bfda725f3ba6f4376b220a3
EC2 Default User <ec2-user@ip-172-31-42-15.ec2.internal>
2022-02-09 19:41:29 +0000

    lab 3and lecture 3

---
+++ lec3.py
@@ -0,0 +1,21 @@
+print('hello\tworld')
+
+print('hello\nworld')
+
+print(  len('hello world'  ) )
+
+print('hello world' [2])
+
+my_letter_list = ['a', 'a', 'b','b', 'c']
+print(my_letter_list)
+
+print( set(my_letter_list)  )
+
+my_unique_letters = set(my_letter_list)
+print(my_unique_letters)
+
+print( len(my_unique_letters ) )
+
+print('d' in my_unique_letters)
+
+print( list (my_unique_letters)[0])
